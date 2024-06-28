9ec32d8aa9738d1d31c2d3625c0bb8a643287fa3
Louis <louis.drotos@gmail.com>
2024-06-27 20:31:04 -0400

    Added quasar to application

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
 import App from './App.vue'
+import '@quasar/extras/mdi-v7/mdi-v7.css'
 
-createApp(App).mount('#app')
+const myApp = createApp(App)
+
+myApp.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+})
+
+myApp.mount('#app')
