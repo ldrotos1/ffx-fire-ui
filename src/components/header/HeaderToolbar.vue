08011779852dd7d02de39fc77361bad159fbdc28
Louis <louis.drotos@gmail.com>
2024-07-18 20:44:58 -0400

    Moved app header toolbar to seperate component and added homepage link

---
+++ src/components/header/HeaderToolbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-toolbar>
+    <q-toolbar-title>
+      <q-avatar square>
+        <img v-bind:src="img" class="pointer" :onclick="gotoHomePage">
+      </q-avatar>
+      <span class="q-ml-sm pointer" :onclick="gotoHomePage">Fairfax County Fire and Rescue Operations</span>
+    </q-toolbar-title>
+  </q-toolbar>
+</template>
+
+<script setup>
+import img from 'assets/patches/ffx-fire.png'
+import { useRouter } from 'vue-router';
+
+const router = useRouter()
+
+function gotoHomePage() {
+  router.push({ path: "/" })
+}
+</script>
+
+<style lang="scss" scoped>
+.pointer {
+  cursor: pointer;
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,14 +2,7 @@
   <q-layout view="hHh lpR fFf">
 
     <q-header elevated class="bg-primary text-white" height-hint="98">
-      <q-toolbar>
-        <q-toolbar-title>
-          <q-avatar square>
-            <img :src="getImage">
-          </q-avatar>
-          Fairfax County Fire and Rescue Operations
-        </q-toolbar-title>
-      </q-toolbar>
+      <HeaderToolbar />
 
       <q-tabs align="left">
         <q-route-tab to="/stations" label="Stations" />
@@ -24,17 +17,8 @@
   </q-layout>
 </template>
 
-<script>
-import fairfaxFireImg from "../assets/patches/ffx-fire.png"
-
-export default {
-  name: "HomePageLayout",
-  computed: {
-    getImage() {
-      return fairfaxFireImg
-    }
-  }
-}
+<script setup>
+import HeaderToolbar from "components/header/HeaderToolbar.vue";
 </script>
 
 <style lang="scss" scoped></style>
